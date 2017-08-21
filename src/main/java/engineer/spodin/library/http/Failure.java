8923efe2b81a2a319c1edda280099c69b5afbdd1
Vasiliy Spodin <v@spodin.engineer>
2017-08-21 14:03:49 +0300

    Make `Failure` class immutable with default message

--- src/main/java/engineer/spodin/library/http/Failure.java
+++ src/main/java/engineer/spodin/library/http/Failure.java
@@ -1,7 +1,7 @@
 package engineer.spodin.library.http;
 
 /**
- * Represents occurred failure optionally enriched with getCode.
+ * Immutable exception that represents occurred failure, optionally enriched with code.
  *
  * <p>Implemented to handle failures in a generic way.
  *
@@ -9,36 +9,39 @@ package engineer.spodin.library.http;
  * that maps to <a href="https://tools.ietf.org/html/rfc7231#section-6.6.1">Internal Server Error</a>
  * HTTP status.
  */
-public class Failure extends RuntimeException {
+public final class Failure extends RuntimeException {
     private static final int DEFAULT_CODE = 500; // internal server error
+    private static final String DEFAULT_MESSAGE = "No message provided";
 
-    private int code = DEFAULT_CODE;
+    private final int code;
+
+    public Failure(int code) {
+        this(code, DEFAULT_MESSAGE, null);
+    }
 
     public Failure(String message) {
-        super(message);
-    }
-
-    public Failure(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public Failure(Throwable cause) {
-        super(cause);
+        this(DEFAULT_CODE, message, null);
     }
 
     public Failure(int code, String message) {
-        super(message);
-        setCode(code);
+        this(code, message, null);
+    }
+
+    public Failure(int code, Throwable cause) {
+        this(code, DEFAULT_MESSAGE, cause);
+    }
+
+    public Failure(String message, Throwable cause) {
+        this(DEFAULT_CODE, message, cause);
+    }
+
+    public Failure(Throwable cause) {
+        this(DEFAULT_CODE, DEFAULT_MESSAGE, cause);
     }
 
     public Failure(int code, String message, Throwable cause) {
         super(message, cause);
-        setCode(code);
-    }
-
-    public Failure(int code, Throwable cause) {
-        super(cause);
-        setCode(code);
+        this.code = code;
     }
 
     /**
@@ -50,26 +53,28 @@ public class Failure extends RuntimeException {
         return code;
     }
 
-    private void setCode(int code) {
-        this.code = code;
-    }
-
     /**
-     * Wraps specified {@link Throwable} to failure instance.
+     * Wraps specified {@link Throwable} to {@link Failure} instance.
      *
      * <p>If specified one isn't instance of {@link Failure} - than
      * new instance will be constructed using {@code cause.getMessage()}
      * and {@code cause.getCause()} from passed.
      *
+     * <p>If specified is {@code null} - new instance will be constructed
+     * with default code and message.
+     *
      * @param cause non-null cause
      * @return failure instance
      */
     public static Failure wrap(final Throwable cause) {
+        if (cause == null) {
+            return new Failure(DEFAULT_MESSAGE);
+        }
         if (cause instanceof Failure) {
             return (Failure) cause;
         }
         if (cause.getMessage() == null) {
-            return new Failure("No message provided", cause.getCause());
+            return new Failure(DEFAULT_MESSAGE, cause.getCause());
         }
         return new Failure(cause.getMessage(), cause.getCause());
     }
