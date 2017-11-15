5b7074b668c22b017dfd957499d9db67addf73fb
Vasiliy Spodin <v@spodin.engineer>
2017-11-15 16:50:32 +0200

    Rename field variable
    This introduce more precise name and avoid hiding with constructor-local variable.

--- src/main/java/engineer/spodin/library/graphql/web/Response.java
+++ src/main/java/engineer/spodin/library/graphql/web/Response.java
@@ -9,7 +9,7 @@ import java.util.*;
  * ready to be sent over wire.
  */
 final class Response implements Map<String, Object> {
-    private final Map<String, Object> result;
+    private final Map<String, Object> data;
 
     Response(Object data, List<GraphQLError> errors) {
         Map<String, Object> result = new LinkedHashMap<>();
@@ -17,71 +17,71 @@ final class Response implements Map<String, Object> {
             result.put("errors", errors);
         }
         result.put("data", data);
-        this.result = Collections.unmodifiableMap(result);
+        this.data = Collections.unmodifiableMap(result);
     }
 
     @Override
     public int size() {
-        return result.size();
+        return data.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return result.isEmpty();
+        return data.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return result.containsKey(key);
+        return data.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return result.containsKey(value);
+        return data.containsKey(value);
     }
 
     @Override
     public Object get(Object key) {
-        return result.get(key);
+        return data.get(key);
     }
 
     @Override
     public Object put(String key, Object value) {
-        return result.put(key, value);
+        return data.put(key, value);
     }
 
     @Override
     public Object remove(Object key) {
-        return result.remove(key);
+        return data.remove(key);
     }
 
     @Override
     public void putAll(Map<? extends String, ?> m) {
-        result.putAll(m);
+        data.putAll(m);
     }
 
     @Override
     public void clear() {
-        result.clear();
+        data.clear();
     }
 
     @Override
     public Set<String> keySet() {
-        return result.keySet();
+        return data.keySet();
     }
 
     @Override
     public Collection<Object> values() {
-        return result.values();
+        return data.values();
     }
 
     @Override
     public Set<Entry<String, Object>> entrySet() {
-        return result.entrySet();
+        return data.entrySet();
     }
 
     @Override
     public String toString() {
-        return "Response{" + result.toString() + '}';
+        return "Response{" + data.toString() + '}';
     }
 }
